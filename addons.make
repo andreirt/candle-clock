009eaac9d04f6e2340500b65b62296996a17abdc
Victor Fornaciari <victor@mandelbrot.com.br>
2016-03-23 15:35:29 -0300

    upgrade to OF 0.9.3; replaces ofxUI with ofxImGui

--- addons.make
+++ addons.make
@@ -1,2 +1,2 @@
+ofxImGui-master
 ofxXmlSettings
-ofxUI
--- src/ofApp.h
+++ src/ofApp.h
@@ -1,135 +1,74 @@
 #pragma once
 
 #include "ofMain.h"
-#include "ofxUI.h"
+#include "ofxImGui.h"
 
 class ofApp : public ofBaseApp{
-
+    
 private:
-    ofxUICanvas *gui;
-    ofxUICanvas *cameraPanel;
-    ofxUICanvas *imagePanel;
-
+    ofXml settings;
+    ofxImGui*gui;
+    GLuint imageButtonID;
+    static int intervalToSaveImage;
+    static int degreesButtonValue;
+    static int currentResolution;
+    static int columns;
+    static int secondsPerImage;
+    static bool saveImageToggle;
+    static bool showAtStartup;
+    static bool fullscreen;
+    static bool configurationPanelShow;
+    const char* deviceResolution[5];
+    
     std::map<string, string> ptStrings;
     std::map<string, string> enStrings;
     std::map<string, string> currentStrings;
 
-    bool hideButtonReleased;
-
+    string changeLocaleLabel;
+    
     ofVideoGrabber* videoGrabber;
     int selectedCameraIndex;
-
+    
     ofImage screenImage;
-
-    int cameraWidth;
-    int cameraHeight;
-
-    int imageWidth;
-    int imageHeight;
-
     int currentColumn;
     float millisecondsPerPixel;
     float lastTimePixelWasDrawn;
+    float lastTimeImageWasSaved;
     int x;
     int y;
     int columnWidth;
-    int columns;
-    int intervalToSaveImage;
-
+    int rotations;
+    int cameraWidth;
+    int cameraHeight;
+    int imageWidth;
+    int imageHeight;
+    bool saveImage;
+    
+    int currentLocale;
     const static float MAX_STRENGTH_AROUND_PIXEL;
-    const static string SUPPORT_BUTTON_NAME;
     const static string CHANGE_LOCALE_BUTTON_NAME;
-
     const static string ENGLISH_LABEL;
     const static string PORTUGUESE_LABEL;
-
-    int currentLocale;
     const static int LOCALE_ENGLISH = 0;
     const static int LOCALE_PORTUGUESE = 1;
-
-    ofxUILabel* titleLabel;
-    ofxUILabelButton* changeLocaleButton;
-
-    float lastTimeImageWasSaved;
-
-    ofxUIDropDownList* cameraList;
-
-    ofxUILabel* cameraWidthLabel;
-    ofxUITextInput* cameraWidthTextInput;
-
-    ofxUILabel* cameraHeightLabel;
-    ofxUITextInput* cameraHeightTextInput;
-
-    ofxUILabel* columnsLabel;
-    ofxUITextInput* columnsTextInput;
-
-    ofxUILabel* secondsPerImageLabel;
-    ofxUITextInput* secondsPerImageTextInput;
-
-    ofxUITextInput* intervalToSaveTextInput;
-
-    ofxUILabel* imageRotationLabel;
-
-    ofxUIDropDownList* pickCameraLabel;
-
-    ofxUIToggle* zeroRotationToggle;
-    ofxUIToggle* ninetyRotationToggle;
-    ofxUIToggle* oneHundredEightyRotationToggle;
-    ofxUIToggle* twoHundredSeventyRotationToggle;
-    int rotations;
-
-    ofxUILabel* credits1Label;
-    ofxUILabel* credits2Label;
-    ofxUILabel* credits3Label;
-    ofxUILabel* credits4Label;
-    ofxUILabel* credits5Label;
-
-    ofxUILabel* minutesLabel;
-    ofxUILabelButton* clearButton;
-
-    ofxUILabelButton* saveButton;
-    ofxUILabelButton* cancelButton;
-    ofxUILabel* supportLabel;
-
-    ofxUIToggle* showAtStartupToggle;
-    bool showAtStartup;
-
-    ofxUIToggle* fullScreenToggle;
-    bool fullScreen;
-
-    ofxUIToggle* saveImageToggle;
-    bool saveImage;
-
-    std::vector<ofxUITextInput*> textInputs;
-
-    void saveCurrentImage();
+    
     void fillImageWithWhite( ofImage* image );
+    void saveCurrentImage();
     void reset();
     void cancelConfigurationChanges();
     void applyConfigurationChanges();
     void hideConfigurationPanel();
     void showConfigurationPanel();
-    void unfocusAllTextInputs(ofxUITextInput* except);
-    void checkTextInputFocus(ofxUIEventArgs &e);
-
+    void changeLocale();
+    void saveXmlSettings();
+    void loadXmlSettings();
+    void setFullscreen();
+    void selectResolution();
+    
 public:
     void setup();
     void update();
     void draw();
-
+    
     void keyPressed(int key);
-    void keyReleased(int key);
-    void mouseMoved(int x, int y );
-    void mouseDragged(int x, int y, int button);
-    void mousePressed(int x, int y, int button);
-    void mouseReleased(int x, int y, int button);
-    void windowResized(int w, int h);
-    void dragEvent(ofDragInfo dragInfo);
-    void gotMessage(ofMessage msg);
-
-    // ofxUI
-    void exit();
-    void guiEvent(ofxUIEventArgs &e);
-    void cameraPanelEvent(ofxUIEventArgs &e);
-    void imagePanelEvent(ofxUIEventArgs &e);
 };
